a9fa7a1cc614f56fec3c6446c1035228b23df2b3
Anthony LoGreco <aclogreco@gmail.com>
2018-04-30 21:30:18 -0400

    refactor: Implement the parseSandwichJson() method
    Provide an implementation of the parseSandwichJson() method in order to
    be able to use the Sandwich data in the UI. Also, handle JSONExceptions
    thrown by this method.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,51 @@
 package com.udacity.sandwichclub.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    /**
+     * Parses a JSON string and places the parsed data in a Sandwich object.
+     * @param json JSON string
+     * @return Returns a Sandwich object containing the JSON data
+     * @throws JSONException Exception for JSON error
+     */
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        Sandwich sandwich = new Sandwich();
+        JSONObject sandwichJsonObject = new JSONObject(json);
+        JSONObject sandwichNameJsonObject = sandwichJsonObject.getJSONObject("name");
+
+        sandwich.setMainName(sandwichNameJsonObject.getString("mainName"));
+
+        JSONArray alsoKnownAsJsonArray = sandwichNameJsonObject.getJSONArray("alsoKnownAs");
+        List<String> alsoKnownAsList = new ArrayList<String>();
+        if (alsoKnownAsJsonArray != null) {
+            for (int i = 0; i < alsoKnownAsJsonArray.length(); i++) {
+                alsoKnownAsList.add(alsoKnownAsJsonArray.getString(i));
+            }
+        }
+        sandwich.setAlsoKnownAs(alsoKnownAsList);
+
+        sandwich.setPlaceOfOrigin(sandwichJsonObject.getString("placeOfOrigin"));
+        sandwich.setDescription(sandwichJsonObject.getString("description"));
+        sandwich.setImage(sandwichJsonObject.getString("image"));
+
+        JSONArray ingredientsJsonArray = sandwichJsonObject.getJSONArray("ingredients");
+        List<String> ingredientsList = new ArrayList<String>();
+        if (ingredientsJsonArray != null) {
+            for (int i = 0; i < ingredientsJsonArray.length(); i++) {
+                ingredientsList.add(ingredientsJsonArray.getString(i));
+            }
+        }
+        sandwich.setIngredients(ingredientsList);
+
+        return sandwich;
     }
 }
